35665b1de8b789cd80b6660b5e4eaab853ff3670
Akshay <aksh.p5678@gmail.com>
2026-08-20 19:51:03 +0530

    Update CORS configuration to allow new origin
    allow request from frontend i have deployed

--- backend/src/main/java/com/knowledgeSphere/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/knowledgeSphere/backend/security/SecurityConfig.java
@@ -34,7 +34,12 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
 
-                    corsConfig.setAllowedOrigins(java.util.List.of("http://localhost:3000"));
+                    corsConfig.setAllowedOrigins(
+        java.util.List.of(
+                "http://localhost:3000",
+                "https://khowledge-sphere.vercel.app"
+        )
+);
                     corsConfig.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(java.util.List.of("*"));
                     corsConfig.setAllowCredentials(true);
